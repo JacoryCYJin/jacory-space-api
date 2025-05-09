9629cba0419b906f57dfd80adf687f87073d1d0b
Jacory <jacory.jin@gmail.com>
2025-05-09 21:24:08 +0800

    新建项目

---
+++ src/main/java/org/example/jacoryspaceapi/JacorySpaceApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jacoryspaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacorySpaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacorySpaceApiApplication.class, args);
+    }
+
+}
